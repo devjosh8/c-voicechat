ede95d978b2fdb9d7896e7bd1984b9ef118dcdc0
Joshua Kandel <joshua.8.kandel@gmail.com>
2025-08-14 21:11:57 +0200

    feat: simple RTC DataChannel connection between client and server
    This commit features a simple connection between client and server.
    Before the datachannel is opened however, client and server negotiate
    the details about the future connection over a simple TCP socket. When
    they agree on details about the connection, a RTC connection is
    established.

--- client/src/client.c
+++ client/src/client.c
@@ -3,9 +3,11 @@
 #include <netdb.h>
 #include <netinet/in.h>
 #include <errno.h>
+#include <sched.h>
 #include <signal.h>
 #include <stdatomic.h>
 #include <string.h>
+#include <strings.h>
 #include <time.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -22,21 +24,120 @@
 #define SERVER_ADDRESS "127.0.0.1"
 #define MAXMSG 512
 
+#define MAX_ANSWER 4096
+
 atomic_int running = 1;
+
+void error(const char* msg) {
+  puts(msg);
+  exit(EXIT_FAILURE);
+}
 
 void handle_sigint(int sig) {
   (void) sig;
   running = 0;
 }
 
+void on_track(int peer_connection, int trc_track, void *arg) {
+  printf("Daten vom Server empfangen!\n");
+}
+
+int make_offer_and_get_answer(const char* server_address, int port, char* offer, int offer_length,
+    char* answer_buffer, int max_answer_length) {
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0); // Socket erstellen
+  if(sockfd < 0) return 0;
+  
+  struct sockaddr_in serv_addr;
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(port);
+  serv_addr.sin_addr.s_addr = inet_addr(server_address);
+  
+  if(connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+    printf("Verbindung konnte nicht hergestellt werden. %d\n", errno);
+    return 0;
+  }
+
+  write(sockfd, offer, offer_length);
+
+  // Nachricht empfangen
+  memset(answer_buffer, 0, max_answer_length);
+
+  int read_offer_len = read(sockfd, answer_buffer, max_answer_length);
+
+  close(sockfd);
+
+  return 1;
+}
+
 
 int main() {
   signal(SIGINT, handle_sigint);
-  printf("Client Hello!\n");
+  
+  rtcInitLogger(RTC_LOG_DEBUG, NULL);
+  
+  rtcConfiguration config;
+  config.iceServers = NULL;
+  config.iceServersCount = 0;
+  config.proxyServer = NULL;
+  config.bindAddress = NULL;
+  config.certificateType = RTC_CERTIFICATE_DEFAULT;
+  config.iceTransportPolicy = RTC_TRANSPORT_POLICY_ALL;
+  config.enableIceTcp = 0;
+  config.enableIceUdpMux = 0;
+  config.disableAutoNegotiation = 0;
+  config.forceMediaTransport = 0;
+  config.portRangeBegin = 0;
+  config.portRangeEnd = 0;
+  config.mtu = 0;
+  config.maxMessageSize = 0;
 
-  rtcInitLogger(RTC_LOG_INFO, NULL);
+  int pc = rtcCreatePeerConnection(&config);
+  printf("Peer connection created!\n");
+  
+  // DataChannel erstellen
 
+  int datachannel = rtcCreateDataChannel(pc, "client-to-server");
+  
+  printf("Datachannel was successfully created!\n");
+  // Das hier ist der Client Peer. Er erstellt ein Offer und sendet dieses an den
+  // Server
+
+  char offer[8192];
+  int offer_size = rtcCreateOffer(pc, offer, 8192-1);
+  printf("Offer successfully created. Offer-Size: %d, Offer:\n\n%s\n", offer_size, offer);
+  
+  
+
+  // Offer in einer Datei für den Server abspeichern!
+  char server_answer[MAX_ANSWER];
+
+  int ret = make_offer_and_get_answer("127.0.0.1", SERVER_PORT, offer, offer_size, server_answer, MAX_ANSWER-1);
+  if(ret == 0) {
+    rtcCleanup();
+    error("Error while making offer. Closing...\n");
+  }
+
+  rtcSetRemoteDescription(pc, server_answer, "answer");
+  printf("Bekommene Antwort: %s\n", server_answer);
+
+  const char* data = "Hallo Welt!";
+
+  while(running) {
+    sleep(5);
+    rtcSendMessage(datachannel, data, strlen(data));
+    printf("trying to send message...\n");
+  }
+
+  rtcClosePeerConnection(pc);
+
+
+  rtcDeleteDataChannel(datachannel);
+  rtcDeletePeerConnection(pc);
 
   rtcCleanup();
   return 0;
 }
+
--- server/src/server.c
+++ server/src/server.c
@@ -1,3 +1,5 @@
+#include <asm-generic/errno-base.h>
+#include <asm-generic/errno.h>
 #include <asm-generic/socket.h>
 #include <bits/types/struct_timeval.h>
 #include <errno.h>
@@ -7,105 +9,294 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <strings.h>
 #include <sys/socket.h>
 #include <unistd.h>
 #include <netdb.h>
 
 #include <tinycthread/tinycthread.h>
-
+#include <rtc/rtc.h>
 
 #define PORT 4000
 #define CLIENT_PORT 4001
 #define MAXMSG 512
 
-int datagram_socket;
-char message[MAXMSG];
 
 atomic_int running = 1;
+atomic_int thread_id_counter = 1;
 
 void handle_sigint(int sig) {
   (void) sig;
   running = 0;
 }
 
-int start_datagram_socket(void* arg) {
+struct NegotiationArgs {
+  char* offer;
+  int offer_length;
+  int clientfd;
+  int thread_id;
+};
+
+struct ClientContext {
+  int incoming_datachannel_id;
+  int outgoing_datachannel_id;
+};
+
+void on_message(int pc, const char *message, int size, void *user_ptr) {
+  struct ClientContext *client_context = (struct ClientContext *) user_ptr;
+  if(client_context->incoming_datachannel_id < 0)return;
+  printf("Message auf Datachannel %d: %s\n", client_context->incoming_datachannel_id, message);
+}
+
+void on_datachannel_register(int pc, int dc, void *user_ptr) {
+  struct ClientContext *client_context = (struct ClientContext *) user_ptr;
+  client_context->incoming_datachannel_id = dc;
+  rtcSetMessageCallback(dc, on_message);
+  printf("Peer ID %d registriert einen neuen Datachannel ID %d\n\n\n\n\n\n", pc, dc);
+}
+
+int negotiate_and_start_peer_connection(void *args) {
+
+  struct ClientContext client_context;
+  client_context.incoming_datachannel_id = -1;
+
+  printf("Verhandlung starten...\n"); 
+  struct NegotiationArgs *negotiation_args = (struct NegotiationArgs *) args;
+
+  rtcConfiguration config;
+  config.iceServers = NULL;
+  config.iceServersCount = 0;
+  config.proxyServer = NULL;
+  config.bindAddress = NULL;
+  config.certificateType = RTC_CERTIFICATE_DEFAULT;
+  config.iceTransportPolicy = RTC_TRANSPORT_POLICY_ALL;
+  config.enableIceTcp = 0;
+  config.enableIceUdpMux = 0;
+  config.disableAutoNegotiation = 0;
+  config.forceMediaTransport = 0;
+  config.portRangeBegin = 0;
+  config.portRangeEnd = 0;
+  config.mtu = 0;
+  config.maxMessageSize = 0;
+
+  int pc = rtcCreatePeerConnection(&config);
+  printf("Peer connection created!\n");
+
+  int datachannel = rtcCreateDataChannel(pc, "server-to-client");
+  client_context.outgoing_datachannel_id = datachannel;
+
+
+  rtcSetUserPointer(pc, &client_context);
+  rtcSetDataChannelCallback(pc, on_datachannel_register);
+
+  rtcSetRemoteDescription(pc, negotiation_args->offer, "offer");
+
+  char answer[2048];
+  int answer_size = rtcCreateAnswer(pc, answer, 8192);
   
-  struct sockaddr_in server_addr;
-
-  int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
-  if (sockfd < 0) {
-    printf("Server Socket could not be initialized. %s\n", strerror(errno));
-    exit(EXIT_FAILURE);
-  }
-
-  memset(&server_addr, 0, sizeof(server_addr));
-
-
-  // set socket recv timeout to 500ms
-
-  struct timeval tv;
-  tv.tv_sec = 0;
-  tv.tv_usec = 500000;
-  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv)) < 0) {
-    printf("Socket timeout option could not be set.\n");
-    exit(EXIT_FAILURE);
-  }
+  sleep(1);
+  write(negotiation_args->clientfd, answer, answer_size);
   
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-  server_addr.sin_port = htons((unsigned short)PORT);
+  printf("Verhandlung durchgeführt.\n");
 
-  // bind socket 
-
-  if (  bind(sockfd, (const struct sockaddr *)&server_addr, sizeof(server_addr)) < 0  ) {
-    printf("Socket could not be bound.\n");
-    exit(EXIT_FAILURE);
-  }
-  
-  struct sockaddr_in client_addr;
-  int clientlen = sizeof(client_addr);
-
-  memset(&client_addr, 0, clientlen);
-  int n;
-  void* buffer = malloc(MAXMSG);
-
-  printf("Waiting for incoming traffic...\n");
-
-  // Receive and immediately mirror message back to sender
   while(running) {
-    memset(buffer, 0, MAXMSG);
+    sleep(1);
+  }
 
-    n = recvfrom(sockfd, buffer, MAXMSG, 0, (const struct sockaddr *)&client_addr, &clientlen);
-    if(n < 0 && errno != EAGAIN && errno != EWOULDBLOCK) {
-      printf("Error while receiving message.\n");
+  rtcClosePeerConnection(pc);
+  rtcDeleteDataChannel(datachannel);
+
+  rtcDeletePeerConnection(pc);
+
+  rtcCleanup();
+
+  close(negotiation_args->clientfd);
+  return 1;
+}
+
+int start_handshake_socket(void *arg) {
+  struct sockaddr_in serv_addr, cli_addr;
+  int sockfd = socket(AF_INET, SOCK_STREAM, 0); // Socket erstellen
+                                                //
+  if(sockfd < 0) {
+    printf("Handshake socket could not be initialized.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+
+  struct timeval timeout;
+
+  timeout.tv_sec = 1;
+  timeout.tv_usec = 0;
+  if (setsockopt(sockfd, SOL_SOCKET, SO_RCVTIMEO,
+        (char*)&timeout, sizeof(timeout)) < 0) {
+    perror("setsockopt SO_RCVTIMEO");
+  }
+
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_port = htons(PORT);
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  
+  if(bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
+    printf("Could not bind socket. \n");
+    exit(EXIT_FAILURE);
+  }
+
+  listen(sockfd, 5);
+  printf("Waiting for connection...\n");
+  unsigned int client_length = sizeof(cli_addr);
+  while(running) {
+    int newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &client_length);
+    if (newsockfd < 0) {
+      if (errno == EWOULDBLOCK || errno == EAGAIN) {
+        continue;
+      }
+      printf("Error on accepting client...\n");
+      continue;
     }
-    
-    if(n > 0) {
-      struct sockaddr_in replay_address = client_addr;
-      replay_address.sin_port = htons(CLIENT_PORT);
 
-      printf("Received Bytes: %d. Mirroring back...\n", n, buffer);
+    // Socket schickt Anfrage an Server 
+    char offer_buf[2048];
+    memset(offer_buf, 0, sizeof(offer_buf));
 
-      sendto(sockfd, buffer, n, 0, (struct sockaddr*) &replay_address, sizeof(replay_address));
+    int read_offer_len = read(newsockfd, offer_buf, 2048-1);
+
+    if(read_offer_len < 0) {
+      printf("Error reading from socket.\n");
+      close(newsockfd);
+      continue;
+    } else {
+      struct NegotiationArgs negotiation_args;
+      negotiation_args.offer = offer_buf;
+      negotiation_args.offer_length = read_offer_len;
+      negotiation_args.clientfd = newsockfd;
+      negotiation_args.thread_id = atomic_fetch_add(&thread_id_counter, 1);
+
+      thrd_t thread;
+      thrd_create(&thread, negotiate_and_start_peer_connection, (void *)&negotiation_args);
+      thrd_detach(thread);
+
     }
 
   }
-  close(sockfd);
+  
   return 1;
+}
+
+void on_channel_open(int id, void *user_ptr) {
+  printf("Channel with id %d was opened.\n", id);
+}
+
+void on_channel_close(int id, void *user_ptr) {
+  printf("Channel with id %d was closed.\n", id);
+}
+
+int data_channel_id = 0;
+
+void on_data_channel_callback(int pc, int dc, void *user_ptr) {
+  printf("Ein Datachannel wurde registriert mit ID %d\n", dc);
+  data_channel_id = dc;
 }
 
 int main() {
   signal(SIGINT, handle_sigint);
   printf("Starting Server...\n");
+  rtcInitLogger(RTC_LOG_DEBUG, NULL);
   
-  // Create server thread
-  thrd_t server_thread;
-  thrd_create(&server_thread, start_datagram_socket, NULL);
+  thrd_t handshake_thread;
 
+  thrd_create(&handshake_thread, start_handshake_socket, NULL);
+  
   int res;
-  thrd_join(server_thread, &res);
+  thrd_join(handshake_thread, &res);
+  
+  printf("Server beendet.\n");
 
-  printf("Server Thread terminated with code %d.\n", res);
+  /*rtcInitLogger(RTC_LOG_DEBUG, NULL);
+  
+  rtcConfiguration config;
+  config.iceServers = NULL;
+  config.iceServersCount = 0;
+  config.proxyServer = NULL;
+  config.bindAddress = NULL;
+  config.certificateType = RTC_CERTIFICATE_DEFAULT;
+  config.iceTransportPolicy = RTC_TRANSPORT_POLICY_ALL;
+  config.enableIceTcp = 1;
+  config.enableIceUdpMux = 0;
+  config.disableAutoNegotiation = 0;
+  config.forceMediaTransport = 0;
+  config.portRangeBegin = 0;
+  config.portRangeEnd = 0;
+  config.mtu = 0;
+  config.maxMessageSize = 0;
+
+  int pc = rtcCreatePeerConnection(&config);
+  printf("Peer connection created!\n");
+  
+  rtcSetDataChannelCallback(pc, on_data_channel_callback) ;
+
+
+  // Answer für den Server erstellen; Der Server liefert eine Antwort
+  // der Client erstellt ein OFFER -> libdatachannel soll selbst aushandeln,
+  // was gewählt wird
+
+  
+  // Der Server bekommt an dieser Stelle das SDP des Clients
+  
+  printf("Offer vom Client wird gelesen...\n");
+  getchar();
+
+  // Client SDP lesen
+  FILE* offer_fptr = fopen("/home/josh/development/temp/offer_from_client.txt", "r");
+  if(offer_fptr == NULL) {
+    printf("Fehler beim lesen Datei.\n");
+    exit(EXIT_FAILURE);
+  }
+
+  char client_offer[1024];
+  int read = 0;
+  if ( (read = fread(client_offer, sizeof(char), 1024, offer_fptr)) == 0) {
+    printf("Client Offer konnte nicht gelesen werden!\n.");
+  };
+
+  printf("Client Offer wurde erfolgreich mit %d Bytes eingelesen.\n", read);
+
+  rtcSetRemoteDescription(pc, client_offer, "offer");
+
+  char answer[1024];
+  int answer_size = rtcCreateAnswer(pc, answer, 8192);
+
+  // Answer muss zurück an den Client geschickt werden
+  FILE* answer_fptr = fopen("/home/josh/development/temp/answer_from_server.txt", "w");
+  if(answer_fptr == NULL) {
+    printf("Fehler beim erstellen der Datei.\n");
+    exit(EXIT_FAILURE);
+  }
+  
+  fprintf(answer_fptr, "%s", answer);
+  
+  fclose(answer_fptr);
+  printf("Antwort erfolgreich in Datei geschrieben!\n");
+
+  while(running) {
+    sleep(1);
+    if(data_channel_id != 0) {
+      char message_buffer[512];
+      int buffer_size = sizeof(message_buffer);
+      int ret = rtcReceiveMessage(data_channel_id, message_buffer, &buffer_size);
+      if(ret == RTC_ERR_SUCCESS) {
+        printf("Nachricht empfangen! %s\n", message_buffer);
+      }
+    }
+  }
+
+  rtcClosePeerConnection(pc);
+
+
+  rtcDeletePeerConnection(pc);
+
+  rtcCleanup();*/
   return 0;
 }
 
